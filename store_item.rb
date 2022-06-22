60ade864029ed834abe4283c70dbd64dff5fccfb
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-06-22 09:50:36 -0700

    represent data as a class instance

--- store_item.rb
+++ store_item.rb
@@ -1,42 +1,9 @@
-# #Ruby syntax
-# apple = {:color => "red", :price => 1.00, :weight => "150g" }
-# banana = {:color => "yellow", :price => 0.50, :weight =>"150g"}
-# orange = {:color => "orange", :price => 0.75, :weight =>"100g"}
-
-
-
-# #Javascript synax
-# apple = {color: "red", price:  1.00, weight:  "150g" }
-# banana = {color:  "yellow", price:  0.50, weight: "150g"}
-# orange = {color: "orange", price:  0.75, weight: "100g"}
-
-
-# puts "An apple is #{apple[:color]}, it weighs #{apple[:weight]}, and it costs #{apple[:price]}"
-
-
-
-
-
-
-
-
-# #to call items using the hash
-
-# apple = {color: "red", price:  1.00, weight:  "150g" }
-# banana = {color:  "yellow", price:  0.50, weight: "150g"}
-# orange = {color: "orange", price:  0.75, weight: "100g"}
-
-# fruit = Fruit.new("color", "price", "weight")
-
-# apple=Fruit.new(apple[:color], apple[:price], apple[:weight])
-
-# p apple
-
-
-
-#store_item as a class
+#represent fruit data as a class instance 
 
 class Fruit
+  
+  attr_reader :color, :price, :weight
+  
 
   def initialize(color, price, weight)
     
@@ -45,23 +12,23 @@ class Fruit
     @weight = weight 
   end
   
-  def color
-    @color
-  end 
+  # def color
+  #   @color
+  # end 
 
 
-  def price 
-    @price
-  end 
+  # def price 
+  #   @price
+  # end 
 
 
-  def weight=(weight_input)
-    @weight = weight_input
-  end 
+  # def weight=(weight_input)
+  #   @weight = weight_input
+  # end 
 
-  def weight
-    @weight
-  end 
+  # def weight
+  #   @weight
+  # end 
 
 
   def print_info
@@ -82,6 +49,11 @@ banana.print_info
 orange=Fruit.new("orange", "0.75", "100g")
 
 orange.print_info
+
+
+
+
+
 
 
 
