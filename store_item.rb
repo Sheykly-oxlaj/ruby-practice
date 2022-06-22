8144186e16053dfc45bc210685cac989025fd3bc
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-06-22 09:43:35 -0700

    Defining a class, fruit

--- store_item.rb
+++ store_item.rb
@@ -1,17 +1,37 @@
-#Ruby syntax
-apple = {:color => "red", :price => 1.00, :weight => "150g" }
-banana = {:color => "yellow", :price => 0.50, :weight =>"150g"}
-orange = {:color => "orange", :price => 0.75, :weight =>"100g"}
+# #Ruby syntax
+# apple = {:color => "red", :price => 1.00, :weight => "150g" }
+# banana = {:color => "yellow", :price => 0.50, :weight =>"150g"}
+# orange = {:color => "orange", :price => 0.75, :weight =>"100g"}
 
 
 
-#Javascript synax
-apple = {color: "red", price:  1.00, weight:  "150g" }
-banana = {color:  "yellow", price:  0.50, weight: "150g"}
-orange = {color: "orange", price:  0.75, weight: "100g"}
+# #Javascript synax
+# apple = {color: "red", price:  1.00, weight:  "150g" }
+# banana = {color:  "yellow", price:  0.50, weight: "150g"}
+# orange = {color: "orange", price:  0.75, weight: "100g"}
 
 
-puts "An apple is #{apple[:color]}, it weighs #{apple[:weight]}, and it costs #{apple[:price]}"
+# puts "An apple is #{apple[:color]}, it weighs #{apple[:weight]}, and it costs #{apple[:price]}"
+
+
+
+
+
+
+
+
+# #to call items using the hash
+
+# apple = {color: "red", price:  1.00, weight:  "150g" }
+# banana = {color:  "yellow", price:  0.50, weight: "150g"}
+# orange = {color: "orange", price:  0.75, weight: "100g"}
+
+# fruit = Fruit.new("color", "price", "weight")
+
+# apple=Fruit.new(apple[:color], apple[:price], apple[:weight])
+
+# p apple
+
 
 
 #store_item as a class
@@ -19,11 +39,12 @@ puts "An apple is #{apple[:color]}, it weighs #{apple[:weight]}, and it costs #{
 class Fruit
 
   def initialize(color, price, weight)
+    
     @color = color
-    @orice = price 
+    @price = price 
     @weight = weight 
   end
-
+  
   def color
     @color
   end 
@@ -38,16 +59,32 @@ class Fruit
     @weight = weight_input
   end 
 
+  def weight
+    @weight
+  end 
+
+
+  def print_info
+    puts "This fruit is #{@color}, it weighs #{@weight}, and it costs #{@price}"
+  end 
+
 end 
 
-apple = {color: "red", price:  1.00, weight:  "150g" }
-banana = {color:  "yellow", price:  0.50, weight: "150g"}
-orange = {color: "orange", price:  0.75, weight: "100g"}
+apple = Fruit.new("red", "1.0", "150g")
 
-fruit = Fruit.new("color", "price", "weight")
+apple.print_info
 
-apple=Fruit.new(apple[:color], apple[:price], apple[:weight])
 
-p apple
+banana=Fruit.new("yellow", "0.50", "150g")
 
-#this is a test comment 
+banana.print_info
+
+orange=Fruit.new("orange", "0.75", "100g")
+
+orange.print_info
+
+
+
+
+
+
