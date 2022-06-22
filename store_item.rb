5eb88fc95e0bb1ec36e167cbed93dc5caa6dfeb2
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-06-22 10:08:43 -0700

    added a name method

--- store_item.rb
+++ store_item.rb
@@ -2,25 +2,26 @@
 
 class Fruit
   
-  attr_reader :color, :price, :weight
+  attr_reader :name, :color, :price, :weight
 
 
   def initialize(input_options)
     
+    @name = input_options[:name]
     @color = input_options[:color]
     @price = input_options[:price]
     @weight = input_options[:weight]
   end
 
   def print_info
-    puts "This fruit is #{color}, it weighs #{weight}, and it costs #{price}"
+    puts "This fruit is a/an #{name}, it is #{color}, it weighs #{weight}, and it costs $#{price}."
   end 
 
 end 
 
-apple = Fruit.new({color: "red", price:  1.00, weight:  "150g" })
-banana = Fruit.new({color:  "yellow", price:  0.50, weight: "150g"})
-orange = Fruit.new ({color: "orange", price:  0.75, weight: "100g"})
+apple = Fruit.new({name: "apple", color: "red", price:  1.00, weight:  "150g" })
+banana = Fruit.new({name: "banana", color:  "yellow", price:  0.50, weight: "150g"})
+orange = Fruit.new ({name: "orange", color: "orange", price:  0.75, weight: "100g"})
 
 
 apple.print_info
