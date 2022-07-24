6b75e2cabeafb38f2f1e5dad6359e02f4e6447ce
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-24 16:42:46 -0700

    adds a solution to problem 3, and a way to reverse sorty without using the method, also, it chains methods

--- ruby-method-hw.rb
+++ ruby-method-hw.rb
@@ -1,47 +1,48 @@
 # # 1. Read about the Ruby sort method. Then sort the following array of hashes by age. (Note - do not use the sort_by method for this exercise)
 # people = [
-  { "name" => "Saron", "age" => 34 },
-  { "name" => "Majora", "age" => 28 },
-  { "name" => "Danilo", "age" => 45 },
-]
-p people.sort { |person_1, person_2| person_1["age"] <=> person_2["age"] }
+#   { "name" => "Saron", "age" => 34 },
+#   { "name" => "Majora", "age" => 28 },
+#   { "name" => "Danilo", "age" => 45 },
+# ]
+# p people.sort { |person_1, person_2| person_1["age"] <=> person_2["age"] }
 
-#Note: This <=> symbol is called “the spaceship operator” & it’s a method you can implement in your class. It should return 1 (greater than), 0 (equal) or -1 (less than).
-#example
-strings = %w(foo test blog a)
+# #Note: This <=> symbol is called “the spaceship operator” & it’s a method you can implement in your class. It should return 1 (greater than), 0 (equal) or -1 (less than).
+# #example
+# strings = %w(foo test blog a)
 
-p strings.sort { |a, b| a.length <=> b.length }
-# --> ["a", "foo", "test", "blog"]
+# p strings.sort { |a, b| a.length <=> b.length }
+# # --> ["a", "foo", "test", "blog"]
 
-#The spaceship operator 
-a <=> b :=
-  if a < b then return -1
-  if a = b then return  0
-  if a > b then return  1
-  if a and b are not comparable then return nil
+# #The spaceship operator
+# a <=> b :=
+#   if a < b then return -1
+#   if a = b then return  0
+#   if a > b then return  1
+#   if a and b are not comparable then return nil
 ##########################################################################################
 
 # # 2. Use the sort method to sort the array of hashes first by age, then by name.
-people = [
-  { name: "bob", age: 15, gender: "male" },
-  { name: "alice", age: 25, gender: "female" },
-  { name: "bob", age: 56, gender: "male" },
-  { name: "dave", age: 45, gender: "male" },
-  { name: "alice", age: 56, gender: "female" },
-  { name: "adam", age: 15, gender: "male" },
-]
-people_sorted_by_age = people.sort { |person_1, person_2| person_1[:age] <=> person_2[:age] }
-people_sorted_by_name = people_sorted_by_age.sort { |person_1, person_2| person_1[:name] <=> person_2[:name] }
+# people = [
+#   { name: "bob", age: 15, gender: "male" },
+#   { name: "alice", age: 25, gender: "female" },
+#   { name: "bob", age: 56, gender: "male" },
+#   { name: "dave", age: 45, gender: "male" },
+#   { name: "alice", age: 56, gender: "female" },
+#   { name: "adam", age: 15, gender: "male" },
+# ]
+# people_sorted_by_age = people.sort { |person_1, person_2| person_1[:age] <=> person_2[:age] }
+# people_sorted_by_name = people_sorted_by_age.sort { |person_1, person_2| person_1[:name] <=> person_2[:name] }
 
-pp people_sorted_by_name
+# pp people_sorted_by_name
 
 # # 3 Use the sort and map methods to convert the array of hashes into an array of titles sorted by their ratings highest to lowest.
-# movies = [
-#   { id: 1, title: "Die Hard", rating: 4.0 },
-#   { id: 2, title: "Bad Boys", rating: 5.0 },
-#   { id: 3, title: "The Chamber", rating: 3.0 },
-#   { id: 4, title: "Fracture", rating: 2.0 },
-# ]
+movies = [
+  { id: 1, title: "Die Hard", rating: 4.0 },
+  { id: 2, title: "Bad Boys", rating: 5.0 },
+  { id: 3, title: "The Chamber", rating: 3.0 },
+  { id: 4, title: "Fracture", rating: 2.0 },
+]
+pp movies.sort { |movie_1, movie_2| movie_2[:rating] <=> movie_1[:rating] }.map { |movie| movie[:title] }
 
 # # 4 Find the needle by writing one line of code. As an example, if haystack = [:hay, :needle, :hay], you'd pull it out with: haystack[1]
 # haystack = { hay: [:hay, :hay, :hay, { hay: { hay: [:hay, { hay: [:hay, :hay, :needle] }, :hay, :hay, :hay] } }, :hay, :hay] }
